7c40cd0b529f3f00151ab4462d90aeef63b76dab
ahmednet <ahmednet03@gmail.com>
2018-08-14 15:16:51 +0200

    preload

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ $(function (){
 	var carouselIndicators = $('.carousel-indicators');
 	var search = $('#search');
 	var btnFilter = $('.btn-filter');
+	//preload function
+	$(window).load(function() {
+		$('.preload').delay(5000).fadeOut('500');
+	});
 	//navbar toggler button function
 	navbarToggler.on('click', function(event) {
 		event.preventDefault();
 		if(navbarToggler.attr('aria-expanded') === 'false')
 		{
-			//$(navbarToggler.attr('data-target')).removeClass('collapse');
+			//$(navbarToggler.attr('data-target')).removeAttr('style')
 			$(navbarToggler.attr('data-target')).addClass(navbarToggler.attr('data-toggle'));
 			navbarToggler.attr('aria-expanded', 'true');
 		} else {
 			navbarToggler.attr('aria-expanded', 'false');
  			$(navbarToggler.attr('data-target')).removeClass(navbarToggler.attr('data-toggle'));
  			$(navbarToggler.attr('data-target')).one('transitionend', function(event) {
-				//$(this).addClass('collapse');
+				//$(this).css('display', 'none');
  			});
 		}
 	});
